6d8712aff2aa2cb8e302dba166fce9db849550e8
Andrew Lloyd <galka1999@gmail.com>
2020-05-20 18:30:39 +0300

    visualize

--- PCA.r
+++ PCA.r
@@ -6,28 +6,31 @@ library(FactoMineR)
 library(ggfortify)
 library(plyr)
 to_analyze <- read_csv('to_analyze.csv')
-only_analyze = select(to_analyze, -binary_diagnosis, -diagnosis, -HDRS, -Y2RS, -cluster_degree, -ID)
+to_analyze = select(to_analyze, -mean_pause, -HDRS, -Y2RS, -cluster_degree)
+only_analyze = select(to_analyze, -binary_diagnosis, -diagnosis, -ID)
 only_analyze %>% 
   scale(center = TRUE, scale = TRUE) -> only_analyze
-dd <- na.omit(select(to_analyze, -HDRS, -Y2RS))
+dd <- na.omit(to_analyze)
 dd$diagnosis = revalue(as.factor(dd$diagnosis), c("1"="schizophrenia", "0"="control", "2"='schizoaffective disorder', '3'='control with schizo-spectrum tendencies'))
 dd <- column_to_rownames(dd, var = "ID")
 ### CA using factoextra & FactoMineR
 autoplot(prcomp(na.omit(only_analyze)), data = dd, 
-         colour = 'diagnosis', label = TRUE, label.size = 5,
-         loadings = TRUE, loadings.colour = 'blue', loadings.label.repel=T,
-         loadings.label = TRUE, loadings.label.size = 5)+ggtitle('Principal Correspondence Analysis using ggfortify, biplot on verbal fluency task with PANSS')
-ggsave("PCA_PANSS_animals.jpeg", width = 50, height = 27, units = "cm")
+         colour = 'diagnosis', label = TRUE, label.size = 3, label.repel = TRUE,
+         loadings = TRUE, loadings.colour = 'black', loadings.label.repel = TRUE,
+         loadings.label = TRUE, loadings.label.size = 3)+ggtitle('Principal Correspondence Analysis using ggfortify, biplot on verbal fluency task with PANSS')+ theme(
+           legend.text = element_text(size = 10))
+ggsave("PCA_PANSS_animals_small.jpeg", width = 35, height = 15, units = "cm")
 
-only_analyze = select(to_analyze, -education, -binary_diagnosis, -diagnosis, -HDRS, -Y2RS, -ID, -PANSS_Total, -PANSS_O, -PANSS_P, -PANSS_N, -TD, -cluster_degree)
+only_analyze = select(to_analyze, -education, -binary_diagnosis, -diagnosis, -PANSS_Total, -PANSS_O, -PANSS_P, -PANSS_N, -TD, -ID, -sex, -age)
 only_analyze %>% 
   scale(center = TRUE, scale = TRUE) -> only_analyze
-dd <- na.omit(select(to_analyze, -HDRS, -Y2RS, -PANSS_Total, -PANSS_O, -PANSS_P, -PANSS_N))
+dd <- na.omit(select(to_analyze, -PANSS_Total, -PANSS_O, -PANSS_P, -PANSS_N, -TD))
 dd$diagnosis = revalue(as.factor(dd$diagnosis), c("1"="schizophrenia", "0"="control", "2"='schizoaffective disorder', '3'='control with schizo-spectrum tendencies'))
 dd <- column_to_rownames(dd, var = "ID")
 ### CA using factoextra & FactoMineR
 autoplot(prcomp(na.omit(only_analyze)), data = dd, 
-         colour = 'diagnosis', label = TRUE, label.size = 5,
-         loadings = TRUE, loadings.colour = 'blue', loadings.label.repel=T,
-         loadings.label = TRUE, loadings.label.size = 5)+ggtitle('Principal Correspondence Analysis using ggfortify, biplot on verbal fluency task')
-ggsave("PCA_animals.jpeg", width = 50, height = 27, units = "cm")
+         colour = 'diagnosis', label = TRUE, label.size = 3, label.repel = TRUE,
+         loadings = TRUE, loadings.colour = 'black', loadings.label.repel = TRUE,
+         loadings.label = TRUE, loadings.label.size = 3)+ggtitle('Principal Correspondence Analysis using ggfortify, biplot on verbal fluency task')+ theme(
+           legend.text = element_text(size = 10))
+ggsave("PCA_animals_small.jpeg", width = 35, height = 15, units = "cm")
--- regression.R
+++ regression.R
@@ -1,43 +1,22 @@
 library(tidyverse)
 to_analyze <- read_csv('to_analyze.csv')
-lm_panss <- lm(PANSS_Total ~ repeat_num:age:sex + no_pos_mean_cos_sim:age:sex + no_pos_min_cos_sim:age:sex + mean_cluster_len:age:sex + mean_pause:age:sex, data = to_analyze)
+lm_panss <- lm(PANSS_Total ~ repeat_num + no_pos_mean_cos_sim + no_pos_min_cos_sim + no_pos_max_cos_sim + unique_num, data = to_analyze)
 summary(lm_panss)
-lm_panss_o <- lm(PANSS_O ~ repeat_num:age:sex + no_pos_mean_cos_sim:age:sex + no_pos_min_cos_sim:age:sex + mean_cluster_len:age:sex + mean_pause:age:sex, data = to_analyze)
+lm_panss_o <- lm(PANSS_O ~ repeat_num + no_pos_mean_cos_sim + no_pos_min_cos_sim + no_pos_max_cos_sim + unique_num, data = to_analyze)
 summary(lm_panss_o)
-lm_panss_p <- lm(PANSS_P ~ repeat_num:age:sex + no_pos_mean_cos_sim:age:sex + no_pos_min_cos_sim:age:sex + mean_cluster_len:age:sex + mean_pause:age:sex, data = to_analyze)
+lm_panss_p <- lm(PANSS_P ~ repeat_num + no_pos_mean_cos_sim + no_pos_min_cos_sim + no_pos_max_cos_sim + unique_num, data = to_analyze)
 summary(lm_panss_p)
-lm_panss_n <- lm(PANSS_N ~ repeat_num:age:sex + no_pos_mean_cos_sim:age:sex + no_pos_min_cos_sim:age:sex + mean_cluster_len:age:sex + mean_pause:age:sex, data = to_analyze)
+lm_panss_n <- lm(PANSS_N ~ repeat_num + no_pos_mean_cos_sim + no_pos_min_cos_sim + no_pos_max_cos_sim + unique_num, data = to_analyze)
 summary(lm_panss_n)
 plot(lm_panss_n)
 
 
-library(cluster)
-library(languageR)
-library(factoextra)
-library(FactoMineR)
-library(ggfortify)
-library(plyr)
-
-only_analyze = select(to_analyze, -binary_diagnosis, -diagnosis, -HDRS, -Y2RS, -cluster_degree, -ID)
-only_analyze %>% 
-  scale(center = TRUE, scale = TRUE) -> only_analyze
-dd <- na.omit(select(to_analyze, -HDRS, -Y2RS))
-dd$diagnosis = revalue(as.factor(dd$diagnosis), c("1"="schizophrenia", "0"="control", "2"='schizoaffective disorder', '3'='control with schizo-spectrum tendencies'))
-dd <- column_to_rownames(dd, var = "ID")
-### CA using factoextra & FactoMineR
-autoplot(prcomp(na.omit(only_analyze)), data = dd, 
-         colour = 'diagnosis', label = TRUE, label.size = 5,
-         loadings = TRUE, loadings.colour = 'blue',
-         loadings.label = TRUE, loadings.label.size = 5)+ggtitle('Principal Correspondence Analysis using ggfortify, biplot on verbal fluency task')
-
-only_analyze = select(to_analyze, -education, -binary_diagnosis, -diagnosis, -HDRS, -Y2RS, -ID, -PANSS_Total, -PANSS_O, -PANSS_P, -PANSS_N, -TD, -cluster_degree)
-only_analyze %>% 
-  scale(center = TRUE, scale = TRUE) -> only_analyze
-dd <- na.omit(select(to_analyze, -HDRS, -Y2RS, -PANSS_Total, -PANSS_O, -PANSS_P, -PANSS_N))
-dd$diagnosis = revalue(as.factor(dd$diagnosis), c("1"="schizophrenia", "0"="control", "2"='schizoaffective disorder', '3'='control with schizo-spectrum tendencies'))
-dd <- column_to_rownames(dd, var = "ID")
-### CA using factoextra & FactoMineR
-autoplot(prcomp(na.omit(only_analyze)), data = dd, 
-         colour = 'diagnosis', label = TRUE, label.size = 5,
-         loadings = TRUE, loadings.colour = 'blue',
-         loadings.label = TRUE, loadings.label.size = 5)+ggtitle('Principal Correspondence Analysis using ggfortify, biplot on verbal fluency task')
+lm_panss <- lm(PANSS_Total ~ repeat_num + unique_num + min_cluster_len + mean_cluster_len + max_cluster_len, to_analyze)
+summary(lm_panss)
+lm_panss_o <- lm(PANSS_O ~ repeat_num + unique_num + min_cluster_len + mean_cluster_len + max_cluster_len, to_analyze)
+summary(lm_panss_o)
+lm_panss_p <- lm(PANSS_P ~ repeat_num + unique_num + min_cluster_len + mean_cluster_len + max_cluster_len, to_analyze)
+summary(lm_panss_p)
+lm_panss_n <- lm(PANSS_N ~ repeat_num + unique_num + min_cluster_len + mean_cluster_len + max_cluster_len, to_analyze)
+summary(lm_panss_n)
+plot(lm_panss_n)
